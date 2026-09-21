9cb9413a6f26afa119eeedecc611b64bc1dd70cf
NickolasFerraz1 <nickolas.oferraz@gmail.com>
2026-09-21 19:55:39 -0300

    fix: Arrumamos o .py para passar no Ruff Test (Anteriormente propositalmente errado)

--- Aula2/processamento.py
+++ Aula2/processamento.py
@@ -1,7 +1,3 @@
-import pandas as pd
-import os # Import não utilizado 
-import sys # Import não utilizado 
-
 def limpa_dados(df):
     """
     Limpa os dados do DataFrame, removendo valores nulos e duplicados.
@@ -12,8 +8,6 @@ def limpa_dados(df):
     Retorna:
     pd.DataFrame: O DataFrame limpo.
     """
-    #Variável não utilizada
-    x = 10
     df = df.dropna()  # Remove linhas com valores nulos
     df = df.drop_duplicates()  # Remove linhas duplicadas
     return df
