1a8126d1a6abf65d1f660cb040ec8bb47adc1671
NickolasFerraz1 <nickolas.oferraz@gmail.com>
2026-09-21 18:43:01 -0300

    feat: Versão de ci.yaml da Aula 1 para rodar código .py

---
+++ Aula1/validate_env.py
@@ -0,0 +1,16 @@
+import sys
+
+def main():
+    print("Iniciando validação de ambiente")
+
+    version = sys.version_info
+    print(f"Versão do Python: {version.major}.{version.minor}.{version.micro}")
+
+    if version.major < 3 or (version.major == 3 and version.minor < 8):
+        print("Erro: A versão do Python deve ser 3.8 ou superior.")
+        sys.exit(1)
+    print("Validação de ambiente concluída com sucesso.")
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
