94afbbf1f0c0407da121f01b80dc5fc4c3556827
NickolasFerraz1 <nickolas.oferraz@gmail.com>
2026-09-21 23:36:03 -0300

    Adicionando dummy_train.py e incluindo isso no ci.yaml

--- Aula3/dummy_train.py
+++ Aula3/dummy_train.py
@@ -1,6 +1,7 @@
+import sys
+
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
-import sys
 
 
 def main():
@@ -19,7 +20,7 @@ def main():
         print(f"Modelo treinado com sucesso. Acurácia: {score:.2f}")
         sys.exit(0)
 
-    except Exception as e:
+    except (FileNotFoundError, KeyError, ValueError) as e:
         print(f"Erro durante o treinamento do modelo: {e}")
         sys.exit(1)
 
