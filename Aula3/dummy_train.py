09903ee6bbd4360eed934a0d6eb252390d8c8ced
NickolasFerraz1 <nickolas.oferraz@gmail.com>
2026-09-21 23:33:12 -0300

    Adicionando dummy_train.py e incluindo isso no ci.yaml

--- Aula3/dummy_train.py
+++ Aula3/dummy_train.py
@@ -1,10 +1,11 @@
 import pandas as pd
-import sys
 from sklearn.tree import DecisionTreeClassifier
+import sys
 
 
 def main():
     print("Iniciando treinamento do modelo")
+
     try:
         df = pd.read_csv('dataset_processado.csv')
 
@@ -17,6 +18,7 @@ def main():
         score = clf.score(X, y)
         print(f"Modelo treinado com sucesso. Acurácia: {score:.2f}")
         sys.exit(0)
+
     except Exception as e:
         print(f"Erro durante o treinamento do modelo: {e}")
         sys.exit(1)
